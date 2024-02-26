c62fab8f7db873557ce4db31e2ad6e349218728b
Ashen Prabath <ashenprabath31@gmail.com>
2024-02-26 20:43:44 +0530

    fixed theming on ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ image, title, category, price, rating }) => {
                 </div>
                 <div className='p-4 md:h-1/3 h-1/2 '>
                     <p className='text-base font-semibold'>{title}</p>
-                    <p className='text-sm font-medium'>{category}</p>
+                    <p className='text-sm font-medium text-gray-500'>{category}</p>
                     <div className='md:flex items-center justify-between pt-3'>
                         <p className='text-xl font-semibold'>${price}</p>
                         <div className='pt-1 md:pt-0'>
