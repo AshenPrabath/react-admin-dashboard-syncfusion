968706325d671e8b31734b4bde97349592cca661
Ashen Prabath <ashenprabath31@gmail.com>
2024-03-16 08:59:08 +0530

    changes on orders table

--- src/components/Tables/OrdersTable.jsx
+++ src/components/Tables/OrdersTable.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Search, Toolbar } from '@syncfusion/ej2-react-grids';
 import { ordersGrid } from '../../data/dummy';
+import { useStateContext } from '../../context/ContextProvider';
 
-const OrdersTable = ({dataSource, pageSize=10}) => {
+
+const OrdersTable = ({ dataSource, pageSize = 10 }) => {
+
+  const { dialogVisible, setDialogVisible } = useStateContext();
+    const [currentProduct, setCurrentProduct] = useState(null);
+
+  let grid;
+    let rowData;
+
+    const commands = [
+        {
+            buttonOption: {
+                content: 'Edit Product', cssClass: 'e-flat'
+            }
+        }
+    ];
+
+    const commandClick = (args) => {
+        if (grid) {
+            rowData = args.rowData;
+            if (rowData) {
+                setCurrentProduct(rowData.ProductID);
+                console.log(rowData.ProductID);
+                setDialogVisible(true);
+                document.body.style.overflow = 'hidden';
+            }
+        }
+    }
   return (
     <div>
-        <GridComponent
+      <GridComponent
         id='gridcomp'
         dataSource={dataSource}
         allowPaging={true}
         allowSorting={true}
-        toolbar={['Search' ]}
-        pageSettings={{pageSize:pageSize}}
+        toolbar={['Search']}
+        pageSettings={{ pageSize: pageSize }}
+        commandClick={commandClick}
+        ref={g => grid = g}
         width="auto"
       >
         <ColumnsDirective>
@@ -19,7 +49,7 @@ const OrdersTable = ({dataSource, pageSize=10}) => {
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Search, Toolbar]}/>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Search, Toolbar]} />
       </GridComponent>
     </div>
   )
