7750bc68d7d0b4d9b3d223386f04a8a481b8a703
Ashen Prabath <ashenprabath31@gmail.com>
2024-04-03 16:45:06 +0530

    Minor changes

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Header, TrackingTable } from '../components';
-import { trackingTableData } from '../data/dummy';
+import { trackingData } from '../data/dummy';
 
 
 const Tracking = () => {
     return (
         <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg'>
             <Header category="Page" title="Tracking" />
-            <TrackingTable dataSource={trackingTableData} />
+            <TrackingTable dataSource={trackingData} />
         </div>
     )
 }
