7add3356b943f76fcd4973d414f3f7411902185e
Ashen Prabath <ashenprabath31@gmail.com>
2024-04-13 20:59:31 +0530

    Finished Customers Table Section

---
+++ src/components/Tables/CustomersTable.jsx
@@ -0,0 +1,66 @@
+import { ColumnDirective, ColumnsDirective, CommandColumn, ContextMenu, Edit, ExcelExport, Filter, GridComponent, Inject, Page, PdfExport, Resize, Search, Sort, Toolbar } from '@syncfusion/ej2-react-grids';
+import React, { useState } from 'react';
+import { useStateContext } from '../../context/ContextProvider';
+import { customersGrid , customersData} from '../../data/dummy';
+import OrderInfoModal from '../OrderInfoModal';
+
+
+const CustomersTable = ({ dataSource, pageSize = 10 }) => {
+
+  const {  orderInfodialogVisible, setOrderInfoDialogVisible } = useStateContext();
+  const [currentOrder, setCurrentOrder] = useState(null);
+
+  let grid;
+  let rowData;
+
+  const commands = [
+    {
+      buttonOption: {
+        content: 'Details', cssClass: 'e-flat'
+      }
+    }
+  ];
+
+  const commandClick = (args) => {
+    if (grid) {
+      rowData = args.rowData;
+      if (rowData) {
+        setCurrentOrder(rowData.CustomerID);
+        setOrderInfoDialogVisible(true);
+        document.body.style.overflow = 'hidden';
+      }
+    }
+  }
+  return (
+    <div>
+      <GridComponent
+        id='gridcomp'
+        dataSource={dataSource}
+        allowPaging={true}
+        allowSorting={true}
+        toolbar={['Search']}
+        pageSettings={{ pageSize: pageSize }}
+        commandClick={commandClick}
+        ref={(g) => (grid = g)}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+          <ColumnDirective field='Actions' headerText='Actions' width='150' textAlign='Center' commands={commands} />
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Search, Toolbar, CommandColumn]} />
+      </GridComponent>
+      {orderInfodialogVisible ? (
+        <OrderInfoModal
+        currentOrder={currentOrder}
+        />
+      ) :
+        null
+      }
+    </div>
+  )
+}
+
+export default CustomersTable
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Toolbar, Sort, Filter, Edit } from '@syncfusion/ej2-react-grids';
-import { customersData, customersGrid } from '../data/dummy';
-
-import { Header } from '../components';
+import { Header, CustomersTable } from '../components';
+import { customersData } from '../data/dummy';
 
 const Customers = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg'>
       <Header category="Page" title="Customers" />
-      <GridComponent
-        id='gridcomp'
-        dataSource={customersData}
-        allowPaging={true}
-        allowSorting={true}
-        toolbar={['Search','Delete', 'Edit']}
-        editSettings={{allowEditing:true, allowDeleting:true}}
-        width="auto"
-      >
-        <ColumnsDirective>
-          {customersGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
-          ))}
-        </ColumnsDirective>
-        <Inject services={[ Page, Toolbar, Selection, Edit, Sort, Filter]}/>
-      </GridComponent>
+      <CustomersTable dataSource={customersData} />
     </div>
   )
 }
